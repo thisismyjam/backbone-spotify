57504505be87138965daff5ed5c9d247a651df0b
Ben Firshman <ben@firshman.co.uk>
2012-09-20 02:55:57 +0100

    Added scroll example

--- example/js/main.js
+++ example/js/main.js
@@ -1,17 +1,30 @@
 
 var IndexView = Backbone.View.extend({
+    initialize: function() {
+        _.bindAll(this, 'onScroll');
+    },
+
     render: function() {
         this.$el.html('Index! <p style="margin-top: 2000px">This is a long page. If you navigate to another page and hit the "back" button in Spotify, it should scroll back to this position.</p>');
         this.restore();
     },
 
     restore: function() {
-        // You might want to bind to any global events here
+        // You may want to put this scrolling code in a base view in your app
+        if (!this._scrollPosition) {
+            this._scrollPosition = 0;
+        }
+        $(window).scrollTop(this._scrollPosition);
+        $(window).on('scroll', this.onScroll);
     },
 
     freeze: function() {
-        // You might want to unbind global events etc here
-    }
+        $(window).off('scroll', this.onScroll);
+    },
+
+    onScroll: _.throttle(function() {
+        this._scrollPosition = $(window).scrollTop();
+    }, 250),
 });
 
 var OtherView = Backbone.View.extend({
